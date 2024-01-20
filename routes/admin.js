c216bd0a36804f48c11019fe289dc805b7124c1a
ahmet keser <ahmetkeser006@gmail.com>
2024-01-21 00:12:37 +0300

    admin işlemler referans bölümü

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,17 @@ const router = express.Router()
 
 const db = require("../data/db")
 
+// Hizmet Listesini veritabanından getirir vvv
 router.get("/hizmetler-list", async function(req,res){
     try{
         const[gelenHizmet, ]=await db.execute("select * from hizmetler")
-        return res.render("admin/hizmetler-list",{
+        res.render("admin/hizmetler-list",{
             title:"Hizmet Düzenle",
             gelenHizmet: gelenHizmet
         })
-        res.redirect("admin/hizmet-adit")
     }catch(err){ console.log(err) }
 })
-
+// hizmet listesinden seçilen kaydı düzenleme sayfasına gönderir vvv
 router.get("/hizmet-edit/:hizid", async function(req,res){
     const hizid =req.params.hizid
     // const hizmettitle=req.body.baslik
@@ -34,6 +34,44 @@ router.get("/hizmet-edit/:hizid", async function(req,res){
 
     }
 })
+// hizmet listesinden düzennleme için gelen kaydı veri tabanına gönderir vvv
+router.post("/hizmet-edit/:hizid", async function(req,res){
+    const hizmetid =req.body.hizmetid
+    const hizmettitle=req.body.baslik
+    const hizmettext = req.body.aciklama
+    const hizmetresim = req.body.resim
+    try{
+        const[gelenHizmet, ]=await db.execute("UPDATE hizmetler SET hizmettitle=? , hizmettext=? , hizmetresim=? WHERE hizmetid=?" ,[hizmettitle, hizmettext,hizmetresim, hizmetid ])
+        const gelen=gelenHizmet[0]
+        res.redirect("/admin/hizmetler-list")
+        
+    }catch(err){
+        console.log(err)
+    }
+})
+// hizmet listesinden silinmek istenen kaydı getirip silme onayı alır vvv
+router.get("/hizmet-delete/:hizid"),async function(req,res){
+    const hizmetid=req.params.hizid
+    try{
+        const [gelenHizmet, ] = await db.execute("select hizmetler From Where hizmetid=?", [hizmetid])
+        const hizmet=gelenHizmet[0]
+        req.render("admin/blog-delete",{
+            title:"Hizmet Silme",
+            hizmet:hizmet
+        })
+    }catch(err){console.log(err)}
+    
+}
+// hizmet listesinden silinmek istenen kaydı getirip silme onayı veri tabanına göndererek silme işlemini tamamlar
+router.post("/hizmet-delete/:hizid"), async function(req,res){
+    const hizmetid = req.body.hizmetid
+    console.log(hizmetid)
+    try{
+        await db.execute("DELETE FROM hizmetler WHERE hizmetid=?",[hizmetid])
+        res.redirect("/admin/hizmetler-list")
+    }catch(err){console.log(err)}
+}
+// yeni girilecek hizmet kaydını kullanıcıdan alır
 router.get("/hizmet-add", async function(req,res){
     try{
         res.render("admin/hizmet-add",{
@@ -43,6 +81,7 @@ router.get("/hizmet-add", async function(req,res){
 
     }
 })
+// yeni girilecek hizmet kaydını veri tabanına gönderir
 router.post("/hizmet-add", async function(req,res){
     const hizmettitle=req.body.baslik
     const hizmettext = req.body.aciklama
@@ -53,9 +92,51 @@ router.post("/hizmet-add", async function(req,res){
         res.redirect("/admin/hizmetler-list") // işlem bittikten sonra hizmetler sayfasına yönlendirir.
     }catch(err){console.log(err)}
 })
-router.use("/referanslar-edit",function(req,res){
-    res.render("admin/referanslar-edit")
+// Referanslar Listesini veritabanından getirir vvv
+router.get("/referans-list", async function(req,res){
+    try{
+        const[referanslar, ]=await db.execute("select * from referanslar")
+        res.render("admin/referans-list",{
+            title:"Referans Listesi",
+            referanslar: referanslar
+        })
+    }catch(err){ console.log(err) }
 })
+// Referanslar listesinden seçilen kaydı düzenleme sayfasına gönderir vvv
+router.get("/referans-edit/:refid", async function(req,res){
+    const refid =req.params.refid
+    try{
+        const[gelenveri, ]=await db.execute("select * from referanslar where referansid = ?",[refid])
+        const gelen=gelenveri[0]
+        if(gelen){
+            return res.render("admin/referans-edit",{
+                title:"Referans Düzenle",
+                gelen:gelen,
+            })
+        }
+        res.redirect("admin/referans-list")
+        
+    }catch(err){
+
+    }
+})
+// referans listesinden düzennleme için gelen kaydı veri tabanına gönderir vvv
+router.post("/referans-edit/:refid", async function(req,res){
+    const referansid =req.body.referansid
+    const referanstitle=req.body.baslik
+    const referanstext = req.body.aciklama
+    const referansresim = req.body.resim
+    const referansmodel = req.body.model
+    try{
+        const[gelenv, ]=await db.execute("UPDATE referanslar SET referanstitle=? , referansresim=?, referansmodel=?, referanstext=?  WHERE referansid=?" ,[referanstitle,referansresim,referansmodel, referanstext, referansid ])
+        const gelen=gelenv[0]
+        res.redirect("/admin/referans-list")
+        
+    }catch(err){
+        console.log(err)
+    }
+})
+
 router.use("/index-edit",function(req,res){
     res.render("admin/index-edit")
 })
